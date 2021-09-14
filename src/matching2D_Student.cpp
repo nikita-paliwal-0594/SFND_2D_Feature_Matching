1833a4206c15db9fad5d6a41e8af44dd2fe976e5
nikita-paliwal-0594 <90314898+nikita-paliwal-0594@users.noreply.github.com>
2021-09-14 22:48:40 +0530

    Add files via upload

--- src/matching2D_Student.cpp
+++ src/matching2D_Student.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 // Find best matches for keypoints in two camera images based on several matching methods
 void matchDescriptors(std::vector<cv::KeyPoint> &kPtsSource, std::vector<cv::KeyPoint> &kPtsRef, cv::Mat &descSource, cv::Mat &descRef,
-                      std::vector<cv::DMatch> &matches, std::string descriptorType, std::string matcherType, std::string selectorType)
+                      std::vector<cv::DMatch> &matches, std::string descriptorCateogy, std::string matcherType, std::string selectorType)
 {
     // configure matcher
     bool crossCheck = false;
@@ -14,10 +14,8 @@ void matchDescriptors(std::vector<cv::KeyPoint> &kPtsSource, std::vector<cv::Key
     if (matcherType.compare("MAT_BF") == 0)
     {
         int normType = cv::NORM_HAMMING;
-		if(descriptorType.compare("DES_HOG") == 0)
-        {
+		if(descriptorCateogy.compare("DES_HOG") == 0)
             normType = cv::NORM_L1;
-        }
         matcher = cv::BFMatcher::create(normType, crossCheck);
     }
     else if (matcherType.compare("MAT_FLANN") == 0)
